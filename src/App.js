263e9ed8001fa58edfec9fddc344df09d0bbca0c
ahmad yahya <ahmadyahyay@gmail.com>
2021-06-29 06:41:41 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {increment,decrement} from './actions'
+function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+  return (
+    <div className="App">
+      <h1>Counter {counter} </h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
+
+export default App;
